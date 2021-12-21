e633e698645bf89233fa08c2a51765a90d2a2572
jeff <jeffyjkang@gmail.com>
2021-12-21 16:48:58 -0600

    api_redirect

---
+++ src/routes/callback.ts
@@ -0,0 +1,40 @@
+const clientId = import.meta.env.VITE_CLIENT_ID
+const secret = import.meta.env.VITE_CLIENT_SECRET
+const tokenURL = 'https://github.com/login/oauth/access_token'
+const userURL = 'https://api.github.com/user'
+
+export async function get(request) {
+  const code = request.query.get('code')
+  const token = await getToken(code)
+  const user = await getUser(token)
+  return {
+    body: JSON.stringify(user, null, 2)
+  }
+}
+
+function getUser(token) {
+  return fetch(userURL, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: `Bearer ${token}`
+    }
+  })
+  .then((res) => res.json());
+}
+
+function getToken(code) {
+  return fetch(tokenURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      client_id: clientId,
+      client_secret: secret,
+      code
+    })
+  }).then((res) => res.json())
+  .then((res) => res.access_token)
+}
+
